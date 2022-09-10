574431e483ce24007e7effc4245aef3b4ce15b1b
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-10 14:42:12 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - produce the same output
+ *
+ * Description: using the main function
+ * this program prints "whatever comes to mind
+ * Return: 0
+ */
+int main(void)
+{
+	char c: 
+        int i: 
+	long li: 
+	long long int lli:
+	float f:
+
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+		printf("Size of an int: %i byte(s)\n", sizeof(int));
+			printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+				printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+					printf("Size of a float: %i byte(s)\n", sizeof(float));
+						return (0);
+}
