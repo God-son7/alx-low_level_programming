ec23cf18a633b5a279d93e81c84bf68775597d15
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-10 15:04:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - produce the same output
+ * main - print the string in the put function
  *
  * Description: using the main function
  * this program prints "whatever comes to mind
