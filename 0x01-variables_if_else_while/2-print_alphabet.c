4200b3463d2c4a6aa0954fae43791af02321f5c5
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-11 09:15:46 -0700

    WARNING: Missing a blank line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main - print if the number is positive 
+ * main - print if the number is positive
  *
- *Description: using the main function 
+ * Description: using the main function
  * this program prints "programming is the best
  * Return: 0
  */
 int main(void)
 {
 	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++);
 	{
 		putchar(ch);
 	}
