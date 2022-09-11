26e114fe4fa1561654ee7d2cd666af5485edd7f6
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-11 08:56:26 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print if the number is positive 
+ *
+ * Description: using the main function 
+ * this program prints "programming is the best
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= '2' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
