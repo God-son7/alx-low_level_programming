38649f04dfe6ec3e312343d9f97fb3128af706b7
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-12 15:04:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this is to print right alphabet
+ *
+ * Description: this is for the alphabet
+ * this program prints "the right alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+	return (0)	
