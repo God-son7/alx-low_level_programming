5c0cc09ffc26ebd5a615f599a2dc68d59c449855
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-12 15:34:06 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,8 +11,11 @@
 int main(void)
 {
 	char ch;
-	for (ch = 'a' ; 'A' ; ch <= 'z' ; 'Z' ; ch++)
-
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{
 		putchar(ch);
 	}
