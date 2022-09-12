f2a440e571d946f492a4305b431d76ae3f8a55fa
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-12 16:36:49 -0700

    ee

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - using my skills
+ *
+ * Description: print numbers
+ * this program prints "numbers in base
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	for (num = '0' ; num <= '9' num++)
+	{
+		printf(num)
+	}
+	return (0);
+}
+
