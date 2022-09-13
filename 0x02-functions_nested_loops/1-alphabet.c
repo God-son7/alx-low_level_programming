ae6d27e38d10765b47fea1b15f831502b6f935b4
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-13 12:59:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - no lele
+ *
+ * Description: vss
+ * this program prints "ggg
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char c;
+	for (c = 'a' ; c <= 'z' ; c++)
+
+		_putchar(c);
+	_putchar('\n');
+
+}
