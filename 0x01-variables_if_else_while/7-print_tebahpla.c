8fa33551aa52c6f0e8df037fff469b05d7d62b61
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-12 17:03:21 -0700

    ee

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - God is with me
+ *
+ * Description: yeshua is the Greatest
+ * this programs prints "i am God son
+ */
+int main(void)
+{
+	int x
+	for (x = 'z' ; x >= 'a' ; x++)
+	{
+		printf("%i" '\n', x);
+	}
+	return (0);
+}
