ba51024a5d09c41f2a689b57c1e04705e35ec219
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-13 13:56:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints 
+ * print_alphabet_x10 - prints
  *
  * return: 0
  */
@@ -9,8 +9,11 @@ void print_alphabet_x10(void)
 	char c, i;
 
 	for (i = 0 ; i <= 9 ; i++)
+	{
 	for (c = 'a' ; c <= 'z' ; c++)
-
+	{
 	_putchar(c);
+	}
 	_putchar('\n');
+	}
 }
