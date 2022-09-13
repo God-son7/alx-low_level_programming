313a91716a82ea282046941a8a8291b63111aec2
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-13 12:08:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - putchar
+ *
+ * Description: game of thrones
+ * this prodram prints "the 
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0 ; i = 8 ; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
