9fc52d47098f3111a125b59bb0d064a622919000
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-12 17:32:37 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - naso
+ *
+ * Descriptipn: let
+ * this program prints "yes
+ * Return: o
+ */
+int main(void)
+{
+	char x;
+	for (x = '0' ; x <= '9' ; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a' ; x <= 'f' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
