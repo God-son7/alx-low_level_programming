9d7460964977155bf8f8ef0ec4d2295eb2c6e17d
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-16 12:28:55 -0700

    jesus

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,9 +13,10 @@ void print_square(int size)
 		{
 			for (; i < size; i++)
 			{
-				for (j = 0; j < size; j++)														 	_putchar(35);
+				for (j = 0; j < size; j++)
+					_putchar(35);
              			_putchar('\n');
-																				}
+			}
 		}
 		else
 		      _putchar('\n');
