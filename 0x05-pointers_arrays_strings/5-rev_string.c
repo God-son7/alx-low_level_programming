ee80caea7eebd24e366b63058440dc742fbe6d8d
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-20 04:46:21 -0700

    hallelujah

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include "main.h"
 
 /**
@@ -20,11 +19,12 @@ void rev_string(char *s)
 	}
 	size = i - 1;
 	half = size / 2;
-	while (half >= 0)	
+	while (half >= 0)
 	{
 		first = s[size - half];
 		last = s[half];
-		s[half] = first;													
-		s[size - half] = last;															half--;
+		s[half] = first;
+		s[size - half] = last;
+		half--;
 	}
 }
