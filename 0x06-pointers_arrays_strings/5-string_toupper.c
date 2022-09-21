e579d737694dfaf2080e7b91d89ea4def1bc409d
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-21 13:49:49 -0700

    i have prospered

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,9 +13,11 @@ char *string_toupper(char *x)
 	length = 0;
 	while (x[length] != '\0')
 	{
-		if (x[length] >= 97 && x[length] <= 122)					{
-			x[length] = x[length] - 32;			
-		}											length++;
+		if (x[length] >= 97 && x[length] <= 122)
+		{
+			x[length] = x[length] - 32;
+		}
+			length++;
 		}
 		return (x);
 }
