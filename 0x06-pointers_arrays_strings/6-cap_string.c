67dc274bb2580e2176eb987fae6b13a163b51d82
olabinjo oluwafemi <westhacker7@gmail.com>
2022-09-21 14:37:26 -0700

    i have prospered

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *s)
 	/*  scan through string */
 	count = 0;
 	while (s[count] != '\0')
-	{/* if next character after count is a char , capitalise it */
+	{
 		if (s[0] >= 97 && s[0] <= 122)
 		{							
 			s[0] = s[0] - 32;
@@ -23,8 +23,8 @@ char *cap_string(char *s)
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'				|| s[count] == ',' || s[count] == ';' || s[count] == '.'			|| s[count] == '.' || s[count] == '!' || s[count] == '?
 			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
     			|| s[count] == '{' || s[count] == '}')
-		{							
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)	
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
 				s[count + 1] = s[count + 1] - 32;
 			}
